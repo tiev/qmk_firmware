027d3e0761fa302d9853928122e5f0fce3d7a2b6
Viet Drake <phuocviet89@gmail.com>
2020-08-11 16:38:34 +0700

    Fix cannot detect left side

--- keyboards/lily58/keymaps/tiev/rules.mk
+++ keyboards/lily58/keymaps/tiev/rules.mk
@@ -13,7 +13,7 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight. 
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_DRIVER_ENABLE = yes    # OLED display
 
@@ -24,7 +24,7 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+        ./lib/keylogger.c
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
