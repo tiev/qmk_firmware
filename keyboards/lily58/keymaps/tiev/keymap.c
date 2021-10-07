856e4ff0c7220edfae71087ba6a29e3f53ae9dee
Viet Tran <phuocviet89@gmail.com>
2021-10-07 16:46:11 +0700

    Change RSHIFT to RCTRL for Workman layout

--- keyboards/lily58/keymaps/tiev/keymap.c
+++ keyboards/lily58/keymaps/tiev/keymap.c
@@ -46,17 +46,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | ESC  |   A  |   S  |   H  |   T  |   G  |-------.    ,-------|   Y  |   N  |   E  |   O  |   I  |  '   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   M  |   C  |   V  |-------|    |-------|   K  |   L  |   ,  |   .  |   /  |RShift|
+ * |LShift|   Z  |   X  |   M  |   C  |   V  |-------|    |-------|   K  |   L  |   ,  |   .  |   /  |RCTRL |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |LCTRL | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_WORKMAN] = LAYOUT( \
-  KC_BSLS, KC_1, KC_2, KC_3, KC_4, KC_5,                   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_EQL,  \
-  KC_TAB,  KC_Q, KC_D, KC_R, KC_W, KC_B,                   KC_J, KC_F, KC_U,    KC_P,   KC_SCLN, KC_MINS, \
-  KC_ESC,  KC_A, KC_S, KC_H, KC_T, KC_G,                   KC_Y, KC_N, KC_E,    KC_O,   KC_I,    KC_QUOT, \
-  KC_LSFT, KC_Z, KC_X, KC_M, KC_C, KC_V, KC_LBRC, KC_RBRC, KC_K, KC_L, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+  KC_BSLS, KC_1, KC_2, KC_3, KC_4, KC_5,                   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_EQL,   \
+  KC_TAB,  KC_Q, KC_D, KC_R, KC_W, KC_B,                   KC_J, KC_F, KC_U,    KC_P,   KC_SCLN, KC_MINS,  \
+  KC_ESC,  KC_A, KC_S, KC_H, KC_T, KC_G,                   KC_Y, KC_N, KC_E,    KC_O,   KC_I,    KC_QUOT,  \
+  KC_LSFT, KC_Z, KC_X, KC_M, KC_C, KC_V, KC_LBRC, KC_RBRC, KC_K, KC_L, KC_COMM, KC_DOT, KC_SLSH, KC_RCTRL, \
                 KC_LCTRL, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_LGUI \
 ),
 
