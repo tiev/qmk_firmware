788816fcea7388005d8c7c62350ff1e9761a7b1f
Viet Tran <phuocviet89@gmail.com>
2021-11-01 12:50:38 +0700

    Use One Shot Shift

--- keyboards/lily58/keymaps/tiev/keymap.c
+++ keyboards/lily58/keymaps/tiev/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_BSLS, KC_1, KC_2, KC_3, KC_4, KC_5,                   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_EQL,  \
   KC_TAB,  KC_Q, KC_W, KC_F, KC_P, KC_B,                   KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN, KC_MINS, \
   KC_ESC,  KC_A, KC_R, KC_S, KC_T, KC_G,                   KC_M, KC_N, KC_E,    KC_I,   KC_O,    KC_ENT,  \
-  KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_LBRC, KC_RBRC, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT, \
+  OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_D, KC_V, KC_LBRC, KC_RBRC, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT, \
           KC_LCTRL, KC_LALT, MO(_LOWER), KC_SPC,  KC_RCTRL, MO(_RAISE), KC_BSPC, KC_LGUI                  \
 ),
 
