c051596d5327f0144faa4fd3250c20a4b493f769
Viet (Drake) Tran <phuocviet89@gmail.com>
2022-03-23 23:28:19 +0700

    Use DF for layer switching

--- keyboards/ymdk/ymd09/keymaps/tiev/keymap.c
+++ keyboards/ymdk/ymd09/keymaps/tiev/keymap.c
@@ -17,21 +17,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT(TALKIE, _______, KC_AUDIO_VOL_UP,
-               _______, _______, KC_AUDIO_VOL_DOWN,
-               LT(1, KC_MEDIA_PLAY_PAUSE), LT(2, KC_F13), KC_AUDIO_MUTE),
+  // General keys
+  [0] = LAYOUT(TALKIE, LSG(KC_A), LSG(KC_O),
+               KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
+               LT(9, KC_MEDIA_PLAY_PAUSE), KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK),
 
-  // Ubuntu
-  // [1] = LAYOUT(KC_F20, _______, KC_F14,
-  //              _______, _______, KC_F15,
-  //              KC_TRNS, _______, KC_F16),
+  // Numpad keys
+  [1] = LAYOUT(KC_KP_7, KC_KP_8, KC_KP_9,
+               KC_KP_4, KC_KP_5, KC_KP_6,
+               LT(9, KC_KP_1), KC_KP_2, LT(2, KC_KP_3)),
 
-  [1] = LAYOUT(LSG(KC_A), _______, KC_CALCULATOR,
-               LSG(KC_O), KC_MEDIA_REWIND, KC_MEDIA_FAST_FORWARD,
-               KC_TRNS, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK),
+  // Numpad extra
+  [2] =  LAYOUT(KC_KP_PLUS, KC_KP_MINUS, KC_KP_ENTER,
+                KC_KP_ASTERISK, KC_KP_SLASH, KC_NUMLOCK,
+                KC_KP_0, KC_KP_DOT, KC_TRNS),
 
-  [2] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
+  // Euro Truck Simulator 2
+  [3] = LAYOUT(KC_1, KC_2, KC_3,
+               KC_TRNS, KC_TRNS, KC_TRNS,
+               LT(9, KC_ESC), KC_M, LT(4, KC_L)),
+
+  // More ETS 2 keys
+  [4] = LAYOUT(KC_F5, KC_F6, KC_F7,
+               KC_F1, KC_F8, KC_F9,
+               KC_TRNS, KC_TRNS, KC_TRNS),
+
+  // RGB Layer
+  [5] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
                RGB_HUI, RGB_VAD, RGB_SAI,
-               RGB_HUD, KC_TRNS, RGB_SAD),
+               LT(9, RGB_HUD), KC_TRNS, RGB_SAD),
+  
+  // Default layer switches
+  [9] = LAYOUT(DF(0), DF(1), DF(3),
+               KC_TRNS, KC_TRNS, KC_TRNS,
+               KC_TRNS, DF(5), KC_TRNS),
 
 };
