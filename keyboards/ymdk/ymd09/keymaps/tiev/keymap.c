ba9907932e12dace3bf3ccbe32f241140ae73d03
Viet (Drake) Tran <phuocviet89@gmail.com>
2022-03-23 23:53:17 +0700

    Name layers for ymd09

--- keyboards/ymdk/ymd09/keymaps/tiev/keymap.c
+++ keyboards/ymdk/ymd09/keymaps/tiev/keymap.c
@@ -4,6 +4,16 @@ enum custom_keycodes {
   TALKIE = SAFE_RANGE,
 };
 
+enum layer_names {
+  _GENERAL,
+  _NUMPAD,
+  _NUMPAD_X,
+  _ETS,
+  _ETS_X,
+  _RGB,
+  _LAYERS,
+};
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case TALKIE:
@@ -18,38 +28,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // General keys
-  [0] = LAYOUT(TALKIE, LSG(KC_A), LSG(KC_O),
+  [_GENERAL] = LAYOUT(TALKIE, LSG(KC_A), LSG(KC_O),
                KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
-               LT(9, KC_MEDIA_PLAY_PAUSE), KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK),
+               LT(_LAYERS, KC_MEDIA_PLAY_PAUSE), KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK),
 
   // Numpad keys
-  [1] = LAYOUT(KC_KP_7, KC_KP_8, KC_KP_9,
+  [_NUMPAD] = LAYOUT(KC_KP_7, KC_KP_8, KC_KP_9,
                KC_KP_4, KC_KP_5, KC_KP_6,
-               LT(9, KC_KP_1), KC_KP_2, LT(2, KC_KP_3)),
+               LT(_LAYERS, KC_KP_1), KC_KP_2, LT(2, KC_KP_3)),
 
   // Numpad extra
-  [2] =  LAYOUT(KC_KP_PLUS, KC_KP_MINUS, KC_KP_ENTER,
+  [_NUMPAD_X] =  LAYOUT(KC_KP_PLUS, KC_KP_MINUS, KC_KP_ENTER,
                 KC_KP_ASTERISK, KC_KP_SLASH, KC_NUMLOCK,
                 KC_KP_0, KC_KP_DOT, KC_TRNS),
 
   // Euro Truck Simulator 2
-  [3] = LAYOUT(KC_1, KC_2, KC_3,
+  [_ETS] = LAYOUT(KC_1, KC_2, KC_3,
                KC_TRNS, KC_TRNS, KC_TRNS,
-               LT(9, KC_ESC), KC_M, LT(4, KC_L)),
+               LT(_LAYERS, KC_ESC), KC_M, LT(4, KC_L)),
 
   // More ETS 2 keys
-  [4] = LAYOUT(KC_F5, KC_F6, KC_F7,
+  [_ETS_X] = LAYOUT(KC_F5, KC_F6, KC_F7,
                KC_F1, KC_F8, KC_F9,
                KC_TRNS, KC_TRNS, KC_TRNS),
 
   // RGB Layer
-  [5] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
+  [_RGB] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
                RGB_HUI, RGB_VAD, RGB_SAI,
-               LT(9, RGB_HUD), KC_TRNS, RGB_SAD),
+               LT(_LAYERS, RGB_HUD), KC_TRNS, RGB_SAD),
   
   // Default layer switches
-  [9] = LAYOUT(DF(0), DF(1), DF(3),
+  [_LAYERS] = LAYOUT(DF(_GENERAL), DF(_NUMPAD), DF(_ETS),
                KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, DF(5), KC_TRNS),
+               KC_TRNS, DF(_RGB), KC_TRNS),
 
 };
